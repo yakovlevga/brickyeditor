33a491945d2615b7212a745f2a6c80aed5870306
George Yakovlev <yakovlev.ga@live.ru>
2018-03-07 09:06:07 +0300

    test-fixes

--- dist/jquery.brickyeditor.es6.js
+++ dist/jquery.brickyeditor.es6.js
@@ -1,3 +1,4 @@
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
--- test/test_helper.js
+++
@@ -1,1 +0,0 @@
-import chai from 'chai';
