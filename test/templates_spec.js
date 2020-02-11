6a251907083e1a89979ebee73a597c887029f289
George Yakovlev <yakovlev.ga@live.ru>
2020-02-11 22:04:07 +0200

    drop unused files

--- test/templates_spec.js
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai';
-import rewire from 'rewire';
-
-describe('Templates tests', () => {
-    const bre = rewire('../dist/brickyeditor.js').__get__('BrickyEditor');
-    
-});
